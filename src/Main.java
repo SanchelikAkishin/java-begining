341f3fa63c6842da93914fd449ff0c2ae1238427
XanderCage05211 <sashuta.akishin@internet.ru>
2024-02-19 22:00:13 +0300

    Тестовое задание для kata.academy.

---
+++ src/Main.java
@@ -0,0 +1,110 @@
+/*Калькулятор умеет выполнять операции сложения, вычитания, умножения и деления с двумя числами: a + b, a - b, a * b, a / b.
+ Данные передаются в одну строку (смотри пример)! Решения, в которых каждое число и арифмитеческая операция передаются с новой строки считаются неверными.
+Калькулятор умеет работать как с арабскими (1,2,3,4,5...), так и с римскими (I,II,III,IV,V...) числами.
+Калькулятор должен принимать на вход числа от 1 до 10 включительно, не более. На выходе числа не ограничиваются по величине и могут быть любыми.
+Калькулятор умеет работать только с целыми числами.
+Калькулятор умеет работать только с арабскими или римскими цифрами одновременно,
+при вводе пользователем строки вроде 3 + II калькулятор должен выбросить исключение и прекратить свою работу.
+При вводе римских чисел, ответ должен быть выведен римскими цифрами, соответственно, при вводе арабских - ответ ожидается арабскими.
+При вводе пользователем неподходящих чисел приложение выбрасывает исключение и завершает свою работу.
+При вводе пользователем строки, не соответствующей одной из вышеописанных арифметических операций, приложение выбрасывает исключение и завершает свою работу.
+Результатом операции деления является целое число, остаток отбрасывается.
+Результатом работы калькулятора с арабскими числами могут быть отрицательные числа и ноль.
+Результатом работы калькулятора с римскими числами могут быть только положительные числа, если результат работы меньше единицы, выбрасывается исключение
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите два числа (Арабские или Римские)");
+        String expression = scanner.nextLine();
+        System.out.println(count(expression));
+    }
+
+    public static String count(String expression) throws Exception {
+        int number;
+        int number1;
+        String oper;
+        String result;
+        boolean isRoman;
+        String[] operands = expression.split("[+\\-*/]");
+        if (operands.length != 2) throw new Exception("Должно быть 2 операции");
+        oper = detectOperation(expression);
+        if (oper == null) throw new Exception("Нет такой операции");
+        if (Roman.isRoman(operands[0]) && Roman.isRoman(operands[1])) {
+            number = Roman.conToArabian(operands[0]);
+            number1 = Roman.conToArabian(operands[1]);
+            isRoman = true;
+        } else if (!Roman.isRoman(operands[0]) && !Roman.isRoman(operands[1])) {
+            number = Integer.parseInt(operands[0]);
+            number1 = Integer.parseInt(operands[1]);
+            isRoman = false;
+        } else {
+            throw new Exception("Цифры разного стиля");
+        }
+        if (number > 10 || number1 > 10) {
+            throw new Exception("Числа больше 10");
+        }
+        int arabian = calt(number, number1, oper);
+        {
+            if (isRoman) {
+                if (arabian <= 0) {
+                    throw new Exception("Число должно быть больше нуля");
+                }
+                result = Roman.conToRoman(arabian);
+            } else {
+                result = String.valueOf(arabian);
+            }
+        }
+        return result;
+    }
+
+    static String detectOperation(String expression) {
+        if (expression.contains("+")) return "+";
+        else if (expression.contains("-")) return "-";
+        else if (expression.contains("*")) return "*";
+        else if (expression.contains("/")) return "/";
+        else return null;
+    }
+
+    static int calt(int a, int b, String oper) {
+        if (oper.equals("+")) return a + b;
+        else if (oper.equals("-")) return a - b;
+        else if (oper.equals("*")) return a * b;
+        else return a / b;
+    }
+}
+
+class Roman {
+    static String[] romanArray = new String[]{"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+            "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI ", "XXVII", "XXVIII", "XXIX", "XXX", " XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", " XXXVII", "XXVIII", "XXXIX", "XL",
+            "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+            "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+            "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+            "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXXVIII", "LXXXIX", "XC",
+            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
+
+    public static boolean isRoman(String val) {
+        for (int i = 0; i < romanArray.length; i++) {
+            if (val.equals(romanArray[i])) ;
+            return true;
+        }
+        return false;
+    }
+
+    public static int conToArabian(String roman) {
+        for (int i = 0; i < romanArray.length; i++) {
+            if (roman.equals(romanArray[i])) ;
+            return i;
+        }
+        return -1;
+    }
+
+    public static String conToRoman(int arabian) {
+        return romanArray[arabian];
+    }
+}
+
